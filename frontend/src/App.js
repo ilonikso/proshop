ddcec7b0678e7a09d776b2e7d7cff6fcf6020023
Illia Onishchenko <pearljam546@gmail.com>
2021-02-15 16:46:42 +0300

    Home screen product listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 import { Container } from 'react-bootstrap'
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
     return (
@@ -8,8 +9,7 @@ const App = () => {
             <Header />
             <main className="py-3">
               <Container>
-                <h2>This is proshop </h2>
-
+                <HomeScreen/>
               </Container>
             </main>
             <Footer />
