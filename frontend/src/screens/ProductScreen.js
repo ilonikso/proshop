b75435bab90846b592bf0d74eb79878c695cb9ad
Illia Onishchenko <pearljam546@gmail.com>
2021-02-15 21:29:11 +0300

    fetch data from server and setup run dev mode

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating/Rating";
-import products from "../products";
+import axios from "axios"
 
 const ProductScreen = ({ match }) => {
-    const product = products.find((p) => p._id === match.params.id);
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/1`);
+
+            setProduct(data);
+        };
+
+        fetchProduct();
+    }, []);
 
     return (
         <>
