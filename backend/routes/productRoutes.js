facb098c9202b616981ba815eee9ec05964dcad8
Illia Onishchenko <pearljam546@gmail.com>
2021-02-16 14:23:51 +0300

    Fetching data from DB

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const AsyncHandler = require("express-async-handler");
+const Product = require("../models/productModel")
+
+const router = express.Router();
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
+
+router.get("/", AsyncHandler( async (req, res) => {
+    const products = await Product.find({})
+
+    res.json(products);
+}));
+
+// @desc    Fetch single products
+// @route   GET /api/products/:id
+// @access  Public
+
+router.get("/:id", AsyncHandler( async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if(product) {
+        res.json(product);
+    } else {
+        res.status(404).json({message: "Product not found"})
+    }
+
+}));
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
-const products = require("./data/products");
 const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
 
 dotenv.config();
 
@@ -14,20 +14,15 @@ app.get("/", (req, res) => {
     res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-    res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+// Using product routes config
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT,
     console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+            .bold
     )
 );
