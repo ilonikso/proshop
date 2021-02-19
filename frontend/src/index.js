763742e9015797561951f4c16d7e6c62c6befd03
Illia Onishchenko <pearljam546@gmail.com>
2021-02-19 21:44:59 +0300

    add/remove from cart actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
-import "./bootstrap.min.css";
 import {Provider} from 'react-redux'
 import store from './redux/store'
 
