bb02999be169aef4e05f5afa49594441e7e0eb49
Illia Onishchenko <pearljam546@gmail.com>
2021-02-22 13:59:03 +0300

    create authMiddleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,30 +1,49 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-const generateToken = require('../utils/generateToken');
+const generateToken = require("../utils/generateToken");
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
-  
-    const user = await User.findOne({ email })
-  
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
     if (user && (await user.matchPassword(password))) {
-      res.json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generateToken(user._id),
-      })
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
     } else {
-      res.status(401)
-      throw new Error('Invalid email or password')
+        res.status(401);
+        throw new Error("Invalid email or password");
     }
-  })
-  
+});
+
+// @desc    Get user profile
+// @route   GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error("User not found");
+    }
+});
 
 module.exports = {
     authUser,
+    getUserProfile,
 };
