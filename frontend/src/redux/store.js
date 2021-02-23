766d51a1823dcb4dfe6ebb61caba84d9c6228a89
Illia Onishchenko <pearljam546@gmail.com>
2021-02-23 20:54:21 +0300

    create login screen

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -4,21 +4,30 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { productListReducer } from "./reducers/productListReducer";
 import { productDetailsReducer } from "./reducers/productDetailsReducer";
 import { CartReducer } from "./reducers/cartReducer";
+import { userLoginReducer } from "./reducers/userReducer";
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: CartReducer,
+    userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
     ? JSON.parse(localStorage.getItem("cartItems"))
     : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null;
+
 const initialState = {
     cart: {
-        cartItems: cartItemsFromStorage
-    }
+        cartItems: cartItemsFromStorage,
+    },
+    userLogin: {
+        userInfo: userInfoFromStorage,
+    },
 };
 
 const middleware = [thunk];
