5dbb6991aa6ea441398b2452a5d0f853db289dca
Illia Onishchenko <pearljam546@gmail.com>
2021-03-05 13:54:04 +0300

    shipping scree, checkout steps compontent

--- frontend/src/redux/actions/cart.js
+++ frontend/src/redux/actions/cart.js
@@ -1,8 +1,12 @@
-import axios from 'axios'
-import {CART_ADD_ITEM, CART_REMOVE_ITEM} from '../types/cart'
+import axios from "axios";
+import {
+    CART_ADD_ITEM,
+    CART_REMOVE_ITEM,
+    CART_SAVE_SHIPPING_ADDRESS,
+} from "../types/cart";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-    const {data } = await axios.get(`/api/products/${id}`)
+    const { data } = await axios.get(`/api/products/${id}`);
 
     dispatch({
         type: CART_ADD_ITEM,
@@ -12,18 +16,33 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
             image: data.image,
             price: data.price,
             countInStock: data.countInStock,
-            qty
-        }
-    })
+            qty,
+        },
+    });
 
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-}
+    localStorage.setItem(
+        "cartItems",
+        JSON.stringify(getState().cart.cartItems)
+    );
+};
 
 export const removeFromCart = (id) => (dispatch, getState) => {
     dispatch({
         type: CART_REMOVE_ITEM,
-        payload: id
-    })
+        payload: id,
+    });
 
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-}
+    localStorage.setItem(
+        "cartItems",
+        JSON.stringify(getState().cart.cartItems)
+    );
+};
+
+export const saveShippingAddress = (data) => (dispatch) => {
+    dispatch({
+        type: CART_SAVE_SHIPPING_ADDRESS,
+        payload: data,
+    });
+
+    localStorage.setItem("shippingAddress", JSON.stringify(data));
+};
