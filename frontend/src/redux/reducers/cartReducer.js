95384b5c06445f8eef50c13ed676ca7c52cf8335
Illia Onishchenko <pearljam546@gmail.com>
2021-03-09 11:49:50 +0300

    create payment screen

--- frontend/src/redux/reducers/cartReducer.js
+++ frontend/src/redux/reducers/cartReducer.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../types/cart";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../types/cart";
 
 export const CartReducer = (state = { shippingAddress: {}, cartItems: [] }, action) => {
     switch (action.type) {
@@ -32,6 +32,12 @@ export const CartReducer = (state = { shippingAddress: {}, cartItems: [] }, acti
             return {
                 ...state,
                 shippingAddress: action.payload
+            }
+        
+        case CART_SAVE_PAYMENT_METHOD:
+            return {
+                ...state,
+                paymentMethod: action.payload
             }
 
         default:
