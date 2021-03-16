e10135c8e4155d72b5f7c69cf78065af0cc6e99b
Illia Onishchenko <pearljam546@gmail.com>
2021-03-16 10:46:26 +0300

    create admin middleware, fetch users list

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { authUser, getUserProfile, registerUser, updateUserProfile } = require("../controllers/userController");
-const protect = require("../middleware/authMiddleware");
-
+const { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } = require("../controllers/userController");
+const { protect, admin } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 router.route("/").post(registerUser);
+router.route("/").get(protect, admin,  getUsers);
 
 module.exports = router;
