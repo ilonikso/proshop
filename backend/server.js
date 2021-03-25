6919b78bcfa9f34415e665cf0a2405bec0fa4124
Illia Onishchenko <pearljam546@gmail.com>
2021-03-25 18:01:27 +0300

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,6 @@ if(process.env.NODE_ENV === 'development'){
 // Get access to read request body
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("API is running");
-});
 
 // Using product routes config
 app.use("/api/products", productRoutes);
@@ -39,6 +36,17 @@ app.use("/api/upload", uploadRoutes);
 
 // paypal config
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/build/index.html')))
+} else {
+    
+    app.get("/", (req, res) => {
+        res.send("API is running");
+    });
+}
 
 // const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
